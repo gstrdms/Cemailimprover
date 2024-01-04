300f3ff534fd04d938d6080d3b0a832264dd3460
gstrdms <hrynzm@outlook.com>
2024-01-04 01:08:38 -0500

    C Sharp email sub improver from Freecodecamp

---
+++ ImproveEmailSubs/Program.cs
@@ -0,0 +1,13 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes below this line here
+
+
+if (daysUntilExpiration <= 11)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+    discountPercentage = 0;
+}
+
