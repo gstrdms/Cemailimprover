a7c1dbc9dbb35dc21b72828fb1f383bd8d952025
gstrdms <hrynzm@outlook.com>
2024-01-15 00:16:47 -0500

    complete

--- ImproveEmailSubs/Program.cs
+++ ImproveEmailSubs/Program.cs
@@ -2,16 +2,26 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-// Your code goes below this line here.
-daysUntilExpiration = 8;
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
 
-     if (daysUntilExpiration >= 11)
-         Console.WriteLine("Your subscription will expire soon. Renew now!");
-
-         else if (daysUntilExpiration >= 6)
-         {
-            Console.WriteLine("Your subscription expires in ",daysUntilExpiration,"days.");
-            Console.WriteLine("Renew now and save ",discountPercentage + 10,"%!");
-         } 
-
-        
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
